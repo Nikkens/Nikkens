edc0c4ec1790c069846efa1a3138bf4ba028d224
Nikkens <96469930+Nikkens@users.noreply.github.com>
2025-07-07 06:46:50 -0400

    Delete Day 1 Project Band Name Generator.py

--- Day 1 Project Band Name Generator.py
+++
@@ -1,4 +0,0 @@
-print("Hello and welcome to the Band Name Generator!\n")
-city = input("First things first,\nWhat is the name of the city you grew up in?\n")
-pet =input("Cool! Nice city, Now name me the name of one of your pets.\n")
-print("Alright, Your new band shall be called... " + city + " " + pet + "!")
